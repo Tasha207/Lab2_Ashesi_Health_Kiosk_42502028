c85aa246d4f9af8ef73f8e214255560dcd7db16b
Tasha207 <nrandzie@gmail.com>
2025-09-29 02:23:15 +0000

    Lab 2 Natalie

--- src/HealthKiosk.java
+++ src/HealthKiosk.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class HealthKiosk {
     public static void main() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter service code (P/L/T/C):");
+        String route;
+        switch{
+            case 'P':
+                route = "Pharmacy Desk";
+            case 'L':
+                route = "Lab Desk";
+        }
 
-        
+
+
+
+
+
 
     }
 }
